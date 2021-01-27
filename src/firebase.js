87c971116c991bbe4074ef67a1202324dc1990fe
jyotisko <jyotisko2006@gmail.com>
2021-01-27 17:24:09 +0530

    change username now available

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "xxxx",
-  authDomain: "xxxx",
-  projectId: "xxxx",
-  storageBucket: "xxxx",
-  messagingSenderId: "xxxx",
-  appId: "xxxxxx"
+  apiKey: "AIzaSyAYJVPSytKyZ0C4Ld4U1EoEV912NHGSBR0",
+  authDomain: "blog-9603a.firebaseapp.com",
+  projectId: "blog-9603a",
+  storageBucket: "blog-9603a.appspot.com",
+  messagingSenderId: "261277740013",
+  appId: "1:261277740013:web:87f86b53270f9be69ed5e4"
 };
 
 export const app = firebase.initializeApp(config);
