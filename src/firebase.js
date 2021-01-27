22c6f71d7faf15036f5e5935df5234c1dd52fd2e
Jyotisko Sengupta <70210028+jyotisko@users.noreply.github.com>
2021-01-27 17:27:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAYJVPSytKyZ0C4Ld4U1EoEV912NHGSBR0",
-  authDomain: "blog-9603a.firebaseapp.com",
-  projectId: "blog-9603a",
-  storageBucket: "blog-9603a.appspot.com",
-  messagingSenderId: "261277740013",
-  appId: "1:261277740013:web:87f86b53270f9be69ed5e4"
+  apiKey: "xxxx",
+  authDomain: "xxx-xxx.xxx.xxx",
+  projectId: "xxx-xxx",
+  storageBucket: "xxx-xxx.xxx.xxx",
+  messagingSenderId: "xxx",
+  appId: "x:xxx:xxx:xxxx"
 };
 
 export const app = firebase.initializeApp(config);
